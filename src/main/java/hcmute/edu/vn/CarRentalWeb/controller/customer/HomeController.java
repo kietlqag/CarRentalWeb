7256d52c7486dafe97cc1fcafd337ca4b2f7b2a8
Le Quoc Kiet <kietlq@gmail.com>
2025-05-13 00:16:21 +0700

    complete service

--- src/main/java/hcmute/edu/vn/CarRentalWeb/controller/customer/HomeController.java
+++ src/main/java/hcmute/edu/vn/CarRentalWeb/controller/customer/HomeController.java
@@ -1,18 +1,51 @@
 package hcmute.edu.vn.CarRentalWeb.controller.customer;
 
 import hcmute.edu.vn.CarRentalWeb.entity.Account;
+import hcmute.edu.vn.CarRentalWeb.entity.Services;
+import hcmute.edu.vn.CarRentalWeb.service.ServicesService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ServicesService servicesService;
+
     @GetMapping("/home")
-    public String home(HttpSession session, Model model) {
+    public String home(@RequestParam(defaultValue = "0") int page,
+                       HttpSession session,
+                       Model model) {
         Account account = (Account) session.getAttribute("account");
         model.addAttribute("account", account);
+
+        Page<Services> services = servicesService.getServicePage("Hoạt động", PageRequest.of(page, 4));
+        model.addAttribute("services", services);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", services.getTotalPages());
+
         return "index";
     }
+
+    @GetMapping("/home/services")
+    public String getPagedServices(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 4; // mỗi trang 3 service
+        Page<Services> servicePage = servicesService.getServicePage("Hoạt động", PageRequest.of(page, pageSize));
+
+        model.addAttribute("services", servicePage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", servicePage.getTotalPages());
+
+        return "fragments/service_list_fragment :: serviceList";
+    }
+
 }
+
