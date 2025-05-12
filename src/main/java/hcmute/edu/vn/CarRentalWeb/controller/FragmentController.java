e2bbb54cfbfabdeedcdff52d87ffebc602e76e56
Le Quoc Kiet <kietlq@gmail.com>
2025-05-12 20:57:33 +0700

    update fragment

---
+++ src/main/java/hcmute/edu/vn/CarRentalWeb/controller/FragmentController.java
@@ -0,0 +1,18 @@
+package hcmute.edu.vn.CarRentalWeb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FragmentController {
+
+    @GetMapping("/staff_sb.html")
+    public String getStaffSidebar() {
+        return "fragments/staff_sb :: sidebar";
+    }
+
+    @GetMapping("/admin_sb.html")
+    public String getAdminSidebar() {
+        return "fragments/admin_sb :: sidebar";
+    }
+}
