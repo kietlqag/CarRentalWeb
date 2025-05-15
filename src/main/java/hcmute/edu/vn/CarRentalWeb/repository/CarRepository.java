48256cd85d8dea2d45c148eac123673579fff28f
Le Quoc Kiet <kietlq@gmail.com>
2025-05-15 22:55:22 +0700

    complete filter

--- src/main/java/hcmute/edu/vn/CarRentalWeb/repository/CarRepository.java
+++ src/main/java/hcmute/edu/vn/CarRentalWeb/repository/CarRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -19,5 +20,18 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
 
     @Query("SELECT DISTINCT c.seat FROM Car c")
     List<Integer> findDistinctSeats();
+
+    @Query("SELECT c FROM Car c WHERE c.status = :status " +
+            "AND (:brand IS NULL OR c.brand = :brand) " +
+            "AND (:seat IS NULL OR c.seat = :seat) " +
+            "AND (:minPrice IS NULL OR c.price >= :minPrice) " +
+            "AND (:maxPrice IS NULL OR c.price <= :maxPrice)")
+    Page<Car> filterCars(@Param("status") String status,
+                         @Param("brand") String brand,
+                         @Param("seat") Integer seat,
+                         @Param("minPrice") Integer minPrice,
+                         @Param("maxPrice") Integer maxPrice,
+                         Pageable pageable);
+
 
 }
