392320e989d706b87eed2544de997b26fb6b7f99
tinhtinh12372 <22110247@student.hcmute.edu.vn>
2025-05-18 23:31:14 +0700

    fix conflict

--- src/main/java/hcmute/edu/vn/CarRentalWeb/service/OrderService.java
+++ src/main/java/hcmute/edu/vn/CarRentalWeb/service/OrderService.java
@@ -9,37 +9,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
     @Autowired
     private OrderRepository orderRepo;
     @Autowired
-    private CarRepository carRepo;
+    CarRepository carRepo;
+
+    public int currentYear = LocalDate.now().getYear();
+    public String status = "Đã hoàn thành";
 
     public List<Order> getAllOrder(){
         return orderRepo.findAll();
     }
-
     public int countOrderByEmail(String email){
         return orderRepo.countByAccountemail(email);
     }
-
+    public int countOrdersByStatus(String status) {
+        return orderRepo.countByStatus(status);
+    }
     public List<Order> getAllOrderByEmail(String email){
         return orderRepo.findAllByAccountemail(email);
     }
-
-    public Order getOrderById(int id) {
-
-        return orderRepo.findOrderById(id);
+    public int countCompletedOrdersThisMonth(){return orderRepo.countCompletedOrdersThisMonth();}
+    public Map<Integer, BigDecimal> getMonthlyRevenue() {
+        List<Object[]> results = orderRepo.findMonthlyRevenueByYearAndStatus(currentYear, status);
+        Map<Integer, BigDecimal> monthlyRevenue = new HashMap<>();
+        for (int i = 1; i <= 12; i++) {
+            monthlyRevenue.put(i, BigDecimal.ZERO);
+        }
+        for (Object[] row : results) {
+            Integer month = ((Number) row[0]).intValue();
+            BigDecimal total = (BigDecimal) row[1];
+            monthlyRevenue.put(month, total);
+        }
+        return monthlyRevenue;
     }
-
+    public BigDecimal getRevenuethisYear() {return orderRepo.getYearlyRevenueByStatus();}
+    public BigDecimal getRevenuethisMonth() {return orderRepo.getMonthlyRevenueByStatus();}
+    public Order getOrderById(int id) {return orderRepo.findOrderById(id);}
+    public List<Order> getRecentOrder() {
+        List<Order> order = orderRepo.findAll();
+        order.sort((o1, o2) -> o2.getCreatedat().compareTo(o1.getCreatedat()));
+        return order.stream().limit(5).collect(Collectors.toList()); // chỉ lấy 5 đơn mới nhất
+    }
     public void save(Order order ) {
 
         orderRepo.save(order);
@@ -60,19 +82,13 @@ public class OrderService {
             order.setCreatedat(LocalDateTime.now());
             order.setName(data.getName());
             order.setService(data.getService() != null ? data.getService() : "Không");
-
-
-
             order.setReceivedate(data.getReceiveDate());
             order.setReturndate(data.getReturnDate());
             order.setCountdate(data.getCountDate());
-
             order.setTotal(data.getTotal());
             order.setDiscount(data.getDiscount());
-
             Car car = carRepo.findCarById(data.getCarid());
             car.setStatus("Đang thuê");
-
             orderRepo.save(order);
             carRepo.save(car);
             return true;
