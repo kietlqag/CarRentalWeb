fd8e88522c568678063b5f00fab588b2b4c2075a
Le Quoc Kiet <kietlq@gmail.com>
2025-05-21 10:05:55 +0700

    conflict moi so ne Thanh Tinh

--- src/main/java/hcmute/edu/vn/CarRentalWeb/repository/NotificationRepository.java
+++ src/main/java/hcmute/edu/vn/CarRentalWeb/repository/NotificationRepository.java
@@ -2,9 +2,11 @@ package hcmute.edu.vn.CarRentalWeb.repository;
 
 import hcmute.edu.vn.CarRentalWeb.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     List<Notification> findAllByAccountemail(String email);
     List<Notification> findTop3ByAccountemailOrderByCreatedatDesc(String email);
